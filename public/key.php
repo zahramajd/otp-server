354eac117b90670cce2a03cf122b2dcaf8416159
zahramajd <zahra.majdabadi95@gmail.com>
2016-07-26 13:02:21 +0430

    4

--- public/key.php
+++ public/key.php
@@ -1,18 +1,38 @@
 <?php require_once '../init.php';
 
-$email = $_GET['email'];
-$pwd = $_GET['pwd'];
+$email = $_POST['email'];
+$pwd = $_POST['pwd'];
+$key = $_POST['key'];
 
 
-$result = $db->users->find([
-    '$and' => [
-        ['email' => $email],
-        ['password' => $pwd],
-    ]
-])->toArray();
+//$result = $db->users->find([
+//    '$and' => [
+//        ['email' => $email],
+//        ['password' => $pwd],
+//    ]
+//])->toArray();
+//
+//if ($result == null) {
+//    echo '[{"error":"Invalid"}]';
+//} else {
+//    echo json_encode($result);
+//}
+//////
 
-if ($result == null) {
-    echo '[{"error":"Invalid"}]';
+echo $email;
+echo $pwd;
+echo $key;
+
+// Check for duplicate email
+$current = $db->users->findOne(['email' => $_REQUEST['email']]);
+if ($current != null) {
+    echo '[{"status":"invalid"}]';
 } else {
-    echo json_encode($result);
+// Insert new user to DB
+    $db->users->insertOne([
+        'email' => $email,
+        'password' => $pwd,
+        'key'=>$key,
+    ]);
+    echo '[{"status":"ok"}]';
 }
