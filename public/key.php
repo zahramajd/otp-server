e89e99dcd9def6f2df9a985301f841922a43cfbe
zahramajd <zahra.majdabadi95@gmail.com>
2016-07-31 13:55:42 +0430

    4

--- public/key.php
+++ public/key.php
@@ -4,24 +4,8 @@ $email = $_POST['email'];
 $pwd = $_POST['pwd'];
 $key = $_POST['key'];
 
+$decoded_key =base64_decode($key);
 
-//$result = $db->users->find([
-//    '$and' => [
-//        ['email' => $email],
-//        ['password' => $pwd],
-//    ]
-//])->toArray();
-//
-//if ($result == null) {
-//    echo '[{"error":"Invalid"}]';
-//} else {
-//    echo json_encode($result);
-//}
-//////
-echo "raftttt";
-echo $email;
-echo $pwd;
-echo $key;
 
 // Check for duplicate email
 $current = $db->users->findOne(['email' => $_REQUEST['email']]);
@@ -32,7 +16,7 @@ if ($current != null) {
     $db->users->insertOne([
         'email' => $email,
         'password' => $pwd,
-        'key'=>$key,
+        'key'=>$decoded_key,
     ]);
     echo '[{"status":"ok"}]';
 }
