1131b214ff1bb1cf2bf7d25f6184a409e94fe038
zahramajd <zahra.majdabadi95@gmail.com>
2016-07-31 13:30:38 +0430

    4

--- public/index.php
+++ public/index.php
@@ -57,6 +57,8 @@ if (isset($_POST['action'])) {
 <body>
 
 
+
+
 <div class="container">
     <div class="card card-container">
         <?php if (isset($message)): ?>
@@ -65,7 +67,7 @@ if (isset($_POST['action'])) {
             </div>
         <?php endif; ?>
 
-        <!--        <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />-->
+                <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
         <p id="profile-name" class="profile-name-card"></p>
 
         <form class="form-signin" method="post">
