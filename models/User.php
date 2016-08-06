6bbeb8391ffea5f9560ed300e2b1258a6b6f9602
zahramajd <zahra.majdabadi95@gmail.com>
2016-08-06 16:59:53 +0430

    4

--- models/User.php
+++ models/User.php
@@ -114,7 +114,8 @@ class User
 //        $generated_otp=$this->key;
 //        return $generated_otp;
       //  $binary_timestamp = pack('N*', 0) . pack('N*', User::get_timestamp());
-        $binary_timestamp=User::get_timestamp();
+       // $binary_timestamp=User::get_timestamp();
+        $binary_timestamp=1470486505983;
 
         $hash = hash_hmac('sha1', $binary_timestamp, base64_decode($this->key), true);
 
