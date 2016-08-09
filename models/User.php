967777133548415ec58c23d8ab7169425d94b12b
zahramajd <zahra.majdabadi95@gmail.com>
2016-08-09 10:52:57 +0430

    test

--- models/User.php
+++ models/User.php
@@ -9,6 +9,8 @@ use MongoDB\BSON\ObjectID;
 class User
 {
 
+
+
     var $data;
 
     public static function find_by_id($id)
@@ -143,8 +145,8 @@ class User
         $value = $value[1];
         // Only 32 bits
         $value = $value & 0x7FFFFFFF;
-        $modulo = pow(10, $this->_codeLength);
-        return str_pad($value % $modulo, $this->_codeLength, '0', STR_PAD_LEFT);
+        $modulo = pow(10,6);
+        return str_pad($value % $modulo, 6, '0', STR_PAD_LEFT);
       //  return $OTP;
     }
 
@@ -154,45 +156,8 @@ class User
         return time();
     }
 
-    function _base32Decode($secret)
-    {
-        if (empty($secret)) return '';
-        $base32chars = $this->_getBase32LookupTable();
-        $base32charsFlipped = array_flip($base32chars);
-        $paddingCharCount = substr_count($secret, $base32chars[32]);
-        $allowedValues = array(6, 4, 3, 1, 0);
-        if (!in_array($paddingCharCount, $allowedValues)) return false;
-        for ($i = 0; $i < 4; $i++){
-            if ($paddingCharCount == $allowedValues[$i] &&
-                substr($secret, -($allowedValues[$i])) != str_repeat($base32chars[32], $allowedValues[$i])) return false;
-        }
-        $secret = str_replace('=','', $secret);
-        $secret = str_split($secret);
-        $binaryString = "";
-        for ($i = 0; $i < count($secret); $i = $i+8) {
-            $x = "";
-            if (!in_array($secret[$i], $base32chars)) return false;
-            for ($j = 0; $j < 8; $j++) {
-                $x .= str_pad(base_convert(@$base32charsFlipped[@$secret[$i + $j]], 10, 2), 5, '0', STR_PAD_LEFT);
-            }
-            $eightBits = str_split($x, 8);
-            for ($z = 0; $z < count($eightBits); $z++) {
-                $binaryString .= ( ($y = chr(base_convert($eightBits[$z], 2, 10))) || ord($y) == 48 ) ? $y:"";
-            }
-        }
-        return $binaryString;
-    }
 
-    function _getBase32LookupTable()
-    {
-        return array(
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', //  7
-            'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', // 15
-            'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', // 23
-            'Y', 'Z', '2', '3', '4', '5', '6', '7', // 31
-            '='  // padding char
-        );
-    }
+
 //    public function makeSecretKey(){
 //
 //        echo 'in method';
