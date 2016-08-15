ef10a3a6a46a57f0c99c4eea4ca70d46655fcef0
zahramajd <zahra.majdabadi95@gmail.com>
2016-08-15 14:11:16 +0430

    test

--- public/key.php
+++ public/key.php
@@ -15,23 +15,26 @@ $seed = base64_encode($random);
 //$success = openssl_public_encrypt($seed, $encrypted, $pb);
 
 
-// Create the keypair
-$res = openssl_pkey_new($config);
-
-// Get private key
-openssl_pkey_export($res, $privKey);
-
-// Get public key
-$pubKey = openssl_pkey_get_details($res);
-$pubKey = $pubKey["key"];
-$pubkey = openssl_get_publickey(base64_decode($pubKey));
-
-//$PubKey = openssl_pkey_get_public($pb);
-$success = openssl_public_encrypt($seed, $encrypted, $pubkey);
+//// Create the keypair
+//$res = openssl_pkey_new($config);
+//
+//// Get private key
+//openssl_pkey_export($res, $privKey);
+//
+//// Get public key
+//$pubKey = openssl_pkey_get_details($res);
+//$pubKey = $pubKey["key"];
+//$pubkey = openssl_get_publickey(base64_decode($pubKey));
+//
+////$PubKey = openssl_pkey_get_public($pb);
+//$success = openssl_public_encrypt($seed, $encrypted, $pubkey);
 
 //$pb=base64_decode($pb);
 //$ok= openssl_public_encrypt($seed,$encrypted,$pb);
 
+
+$pubkey = openssl_pkey_get_public("-----BEGIN PUBLIC KEY----- MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0llCeBjy18RylTdBih9G MUSZIC3GzeN0vQ9W8E3nwy2jdeUnH3GBXWpMo3F43V68zM2Qz5epRNmlLSkY/PJU fJIC8Yc1VEokT52q87hH/XJ5eS8heZnjuSlPAGi8oZ3ImVbruzV7XmlD+QsCSxJW 7tBv0dqJ71e1gAAisCXK2m7iyf/ul6rT0Zz0ptYH4IZfwc/hQ9JcMg69uM+3bb4o BFsixMmEQwxKZsXk3YmO/YRjRbay+6+79bSV/frW+lWhknyGSIJp2CJArYcOdbK1 bXx1dRWpbNSExo7dWwuPC0Y7a5AEeoZofieQPPBhXlp1hPgLYGat71pDqBjKLvF5 GwIDAQAB -----END PUBLIC KEY-----");
+$ok= openssl_public_encrypt($seed,$encrypted,$pubkey);
 
 
 // Check for duplicate email
