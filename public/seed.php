33b7fbcf7d64913c3722dbc18a01ba2ec551e0ba
zahramajd <zahra.majdabadi95@gmail.com>
2016-08-22 11:25:39 +0430

    Totp server

--- public/seed.php
+++ public/seed.php
@@ -21,26 +21,26 @@ $current = User::find_by('email', $email);
 if ($current != null) {
     echo json_encode(['status' => 'already exists']);
     die();
-}
- else {
+} else {
 
-     echo 'in else';
-     // Insert new user to DB
-     db()->users->insertOne([
-         'email' => $email,
-         'password' => $pwd,
-         'seed'=> '',
-     ]);
+    $current = User::find_by('email', $email);
+    echo 'in else';
+    // Insert new user to DB
+    db()->users->insertOne([
+        'email' => $email,
+        'password' => $pwd,
+        'seed' => '',
+    ]);
 
 
 // Make a new seed
-     if (!$current->seed) {
-         $current->seed = sha1("" . (int)(rand(1005, 3234334) * time() / 100));
-     }
+    if (!$current->seed) {
+        $current->seed = sha1("" . (int)(rand(1005, 3234334) * time() / 100));
+    }
 
 // Encrypt seed with key
-     openssl_public_encrypt($current->seed, $encrypted, $key);
-     $encrypted = base64_encode($encrypted);
-     echo $encrypted;
+    openssl_public_encrypt($current->seed, $encrypted, $key);
+    $encrypted = base64_encode($encrypted);
+    echo $encrypted;
 
- }
+}
